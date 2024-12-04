b27b80279f1c1fdeab75a55c2c2b1bb0397b2a68
LAPTOP-RBLOQQKD\Naswa Nida <naswanida3@gmail.com>
2024-12-04 15:08:43 +0700

    tes lagi

--- KRS.java
+++ KRS.java
@@ -2,5 +2,7 @@ public class KRS {
     public static void main(String[] args) {
      System.out.println("Abel cantik"); 
      System.out.println("awowkwkwkwk");  
+     System.out.println("Abel cantik");   
+     System.out.println();
     }
 }
